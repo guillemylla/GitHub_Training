cc085ff8fc968361dc453f58e9a3d3ebc38fb192
guillemylla <guillemyllabou@gmail.com>
2021-02-07 08:50:34 -0500

    add date

--- Hello_world.py
+++ Hello_world.py
@@ -1,10 +1,18 @@
+from datetime import date
+
+
 print("Hello Bob!!!")
 print("Hello, I am your  new branch!")
 
+today = date.today()
+print("Today's date:", today)
+
+
 def sayHI(name):
         "This function says Hi to people"
-        print("Hi, %s" % name )
+        print("Hi, %s, have a nice %s" % (name, today))
 
 
 sayHI("Bob")
 sayHI("Alice")
+
