64fef1f5e438ad9fcf7dd1ee1dbed3fbc1522a94
guillemylla <guillemyllabou@gmail.com>
2021-02-09 14:51:54 -0500

    add function from machine 2

--- File_1.py
+++ File_1.py
@@ -1,6 +1,17 @@
+from datetime import date
+
 print("Hello world!!!")
 print("Hola Mundo!!!")
 
 print("Good morning Bob!") #added from branch a2 
 
 print("Good morning Alice")
+
+
+def sayHi(name):
+	"this function says hi to people"
+	print("Hi, %s, have a nice %s"%(name, today))
+
+today = date.today()
+sayHi("Charles")
+sayHi("David")
